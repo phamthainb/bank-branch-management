c4b3908a6b2c85bdb8e0b14160c6d365e0f5dea0
pham thai <phamthainb@phams-MacBook-Pro.local>
2021-12-09 06:36:32 +0700

    [all] merge code Huy

--- back-end/src/main/java/com/ptit/backend/repository/StaffRepository.java
+++ back-end/src/main/java/com/ptit/backend/repository/StaffRepository.java
@@ -1,6 +1,7 @@
 package com.ptit.backend.repository;
 
 import com.ptit.backend.entity.StaffEntity;
+import com.ptit.backend.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StaffRepository extends JpaRepository<StaffEntity, Long> {
