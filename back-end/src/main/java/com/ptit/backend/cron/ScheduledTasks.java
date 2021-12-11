5944c36cec88a44d555720c12012b6a133392967
phamthainb <phamthai4643@gmail.com>
2021-12-11 09:37:33 +0700

    [fe] revert create customer

--- back-end/src/main/java/com/ptit/backend/cron/ScheduledTasks.java
+++ back-end/src/main/java/com/ptit/backend/cron/ScheduledTasks.java
@@ -1,12 +1,18 @@
 package com.ptit.backend.cron;
 
+import com.ptit.backend.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledTasks {
+    @Autowired
+    AccountService accountService;
+
     @Scheduled(cron = "15 * * * * ?")
     public void scheduleTaskWithCronExpression() {
         System.out.println("Tính lãi hằng ngày....................................................");
+
     }
 }
